4c8c2961f427d1bf3e4966bf2194fee6058bac6f
0nano <35236525+0nano@users.noreply.github.com>
2023-06-06 14:24:51 +0200

    correction p5

--- Partie 5.R
+++ Partie 5.R
@@ -55,3 +55,37 @@ if (stat_test$p.value < alpha) {
   cat("On ne peut pas rejeter l'hypothèse nulle (H0) : tous les etats ont la même vulnérabilité aux séismes.\n")
 }
 
+# On cherche à savoir quels etats ont des distributions de magnitudes de séisme significativement différentes
+# On effectue le test de Wilcoxon pour comparer les distributions des magnitudes de séisme entre chaque paire d'etats
+for (i in 1:(length(groupes))) {
+  for (j in (i + 1):length(groupes)) {
+    stat_test <- wilcox.test(groupes[[i]], groupes[[j]])
+    if (stat_test$p.value < alpha) {
+      cat("Les distributions des magnitudes de séisme sont significativement différentes entre", names(groupes)[i], "et", names(groupes)[j], "\n")
+    }
+  }
+}
+
+# On cherche à savoir quel etat a eu le plus de seismes
+# On calcule le nombre de seismes par etat
+nb_seismes <- sapply(groupes, length)
+
+# On affiche le nombre de seismes par etat
+print(nb_seismes)
+
+# Partie 7 : Comparaison des distributions des seismes entre les etats des États-Unis
+
+# On défini les etats des États-Unis
+etats_US <- c("Alaska", "California", "Hawaii", "Nevada", "Washington", "Arizona", "Idaho", "Montana", "Oregon", "Utah", "Wyoming", "Colorado", "NewMexico", "NorthDakota", "SouthDakota", "Nebraska", "Kansas", "Oklahoma", "Texas", "Arkansas", "Louisiana", "Missouri", "Iowa", "Minnesota", "Wisconsin", "Illinois", "Indiana", "Kentucky", "Michigan", "Ohio", "Tennessee", "Alabama", "Florida", "Georgia", "Mississippi", "NorthCarolina", "SouthCarolina", "Virginia", "WestVirginia", "Delaware", "Maryland", "Pennsylvania", "NewJersey", "NewYork", "Connecticut", "Maine", "Massachusetts", "NewHampshire", "RhodeIsland", "Vermont")
+
+# On filtre les données pour les etats des États-Unis
+seismes_US <- filter(data, pays %in% etats_US)
+
+# On divise les données en groupes pour chaque etat
+groupes_US <- split(seismes_US$mag, seismes_US$pays)
+
+# On effectue le test de student pour comparer les distributions des magnitudes de séisme aux États-Unis
+stat_test_US <- t.test(seismes_US$mag, seismes$mag)
+
+# Afficher les résultats du test statistique
+print(stat_test_US)
